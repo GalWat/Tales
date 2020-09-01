6e2435d2757aa3a6c8c761642c207d071c5573e5
GalWat <watts-galeo@ya.ru>
2020-09-01 22:03:34 +0300

    Generator needs less RAM, test design for folding

--- generator.py
+++ generator.py
@@ -1,26 +1,42 @@
 import os
 from jinja2 import Template
 
-raw_dir = "raw"
 
-names = []
-files = {}
-
-for file in reversed(os.listdir(raw_dir)):
-    file_name, extension = os.path.splitext(file)
-
-    if extension == ".md":
-        path = os.path.join(raw_dir, file)
-
+def next_line(dir):
+    def inner(file):
+        path = os.path.join(dir, file+'.md')
         with open(path, "r", encoding='utf-8') as inp_file:
-            lines = inp_file.read().splitlines()
-            while '' in lines:
-                lines.remove('')
-            names.append((file_name, lines[0]))
-            files[file_name] = lines[1:]
+            inp_file.readline()
+            for line in inp_file:
+                if line not in ['', '\n']:
+                    yield line.strip()
 
-with open('templates\writing.html', 'r', encoding='utf-8') as tmp_file:
-    template = Template(tmp_file.read())
+    return inner
 
-    with open("writing.html", "w", encoding="utf-8") as outp_file:
-        outp_file.write(template.render(names_list=names, files_dict=files))
+
+def get_writing_titles(dir):
+    titles = []
+    for file in reversed(os.listdir(dir)):
+        file_name, extension = os.path.splitext(file)
+        if extension != '.md':
+            continue
+
+        path = os.path.join(dir, file)
+        with open(path, 'r', encoding='utf-8') as inp_file:
+            titles.append((file_name, inp_file.readline().strip()))
+
+    return titles
+
+
+def render_template(template_path, outp, params):
+    with open(template_path, 'r', encoding='utf-8') as tmp_file:
+        template = Template(tmp_file.read())
+
+        with open(outp, 'w', encoding='utf-8') as outp_file:
+            outp_file.write(template.render(params))
+
+
+if __name__ == '__main__':
+    names = get_writing_titles('raw')
+    render_template('templates/writing.html', 'writing.html',
+                    {'names_list': names, 'next_line': next_line('raw')})
