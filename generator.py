2cfbb1d8dee5c7e32e4f7ab0e742505cc0561fc4
GalWat <watts-galeo@ya.ru>
2020-09-01 23:45:03 +0300

    Added folding on "Writing" page

--- generator.py
+++ generator.py
@@ -1,10 +1,11 @@
 import os
+from collections import namedtuple
 from jinja2 import Template
 
 
-def next_line(dir):
-    def inner(file):
-        path = os.path.join(dir, file+'.md')
+def next_line(raw_dir):
+    def inner(file, current_dir=''):
+        path = os.path.join(raw_dir, current_dir, file+'.md')
         with open(path, "r", encoding='utf-8') as inp_file:
             inp_file.readline()
             for line in inp_file:
@@ -15,17 +16,25 @@ def next_line(dir):
 
 
 def get_writing_titles(dir):
-    titles = []
-    for file in reversed(os.listdir(dir)):
-        file_name, extension = os.path.splitext(file)
-        if extension != '.md':
-            continue
+    FoldersNFiles = namedtuple('FoldersNFiles', ['folders', 'files'])
+    content = FoldersNFiles(folders={}, files=[])
 
-        path = os.path.join(dir, file)
-        with open(path, 'r', encoding='utf-8') as inp_file:
-            titles.append((file_name, inp_file.readline().strip()))
-
-    return titles
+    for current_dir, folders, files in os.walk(dir):
+        titles = []
+        prev_dir, folder = os.path.split(current_dir)
+        for file in files:
+            file_name, extension = os.path.splitext(file)
+            if extension == '.md':
+                path = os.path.join(current_dir, file)
+                with open(path, 'r', encoding='utf-8') as inp_file:
+                    titles.append((file_name, inp_file.readline().strip()))
+        
+        if not prev_dir:
+            content.files.extend(titles)
+        else:
+            content.folders[folder] = titles
+    return content
+        
 
 
 def render_template(template_path, outp, params):
@@ -37,6 +46,6 @@ def render_template(template_path, outp, params):
 
 
 if __name__ == '__main__':
-    names = get_writing_titles('raw')
+    content = get_writing_titles('raw')
     render_template('templates/writing.html', 'writing.html',
-                    {'names_list': names, 'next_line': next_line('raw')})
+                    {'content': content, 'next_line': next_line('raw')})
